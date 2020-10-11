2f4584579a404e32b133fa5d576b76b12bf56c09
ezreal_geng <ezreal_geng@163.com>
2020-10-11 11:13:50 +0800

    init

--- springcloud-provider-dept-8001/src/main/java/fun/gengyun/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/fun/gengyun/springcloud/DeptProvider_8001.java
@@ -1,12 +1,9 @@
 package fun.gengyun.springcloud;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 //启动类
 @SpringBootApplication
